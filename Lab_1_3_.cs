3ddb603eceaaa2c669f78ab01647e40e537f4906
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-06-12 23:45:49 +0545

    Implement area and perimeter calculation for rectangle
    Added functionality to calculate area and perimeter of a rectangle based on user input.

--- Lab_1_3_.cs
+++ Lab_1_3_.cs
@@ -1,1 +1,19 @@
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Length:");
+        double length = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter Breadth:");
+        double breadth = Convert.ToDouble(Console.ReadLine());
+
+        double area = length * breadth;
+        double perimeter = 2 * (length + breadth);
+
+        Console.WriteLine("Area = " + area);
+        Console.WriteLine("Perimeter = " + perimeter);
+    }
+}
