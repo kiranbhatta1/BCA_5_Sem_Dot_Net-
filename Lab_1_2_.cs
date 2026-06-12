8170f3060b8749e30b5b60293549d9e6e61ab4c0
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-06-12 17:26:30 +0545

    Implement basic arithmetic operations in Lab_1_2_.cs
    add, subtract, multiply and division of two number using input from command line

--- Lab_1_2_.cs
+++ Lab_1_2_.cs
@@ -1,1 +1,15 @@
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = int.Parse(args[0]);
+        int num2 = int.Parse(args[1]);
+
+        Console.WriteLine("Addition: " + (num1 + num2));
+        Console.WriteLine("Subtraction: " + (num1 - num2));
+        Console.WriteLine("Multiplication: " + (num1 * num2));
+        Console.WriteLine("Division: " + (num1 / num2));
+    }
+}
