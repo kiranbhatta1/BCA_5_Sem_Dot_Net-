837b5ad9be73f35b8698f7fd738f3d4d89bbee5a
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-06-13 13:28:28 +0545

    Add Simple Interest calculation program in C#
    This program calculates the Simple Interest based on user input for principal, rate, and time.

--- Lab_1_5_.cs
+++ Lab_1_5_.cs
@@ -1,1 +1,23 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        // C# program to calculate Simple Interest
+
+        Console.Write("Enter Principal: ");
+        double p = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Rate: ");
+        double r = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Time: ");
+        double t = Convert.ToDouble(Console.ReadLine());
+
+        double si = (p * r * t) / 100;
+
+        Console.WriteLine("Simple Interest: " + si);
+    }
+}
+    
