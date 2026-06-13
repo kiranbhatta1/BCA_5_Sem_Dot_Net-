4a5e68cadbb868c90dadb0fe72ab908f43c4811a
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-06-14 05:44:27 +0545

    Implement program to find the largest of three numbers
    using nested if statement.

--- Lab_2_1_.cs
+++ Lab_2_1_.cs
@@ -1,1 +1,39 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b, c;
+
+        Console.WriteLine("Enter three numbers:");
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        c = Convert.ToInt32(Console.ReadLine());
+
+        if (a > b)
+        {
+            if (a > c)
+            {
+                Console.WriteLine("Largest: " + a);
+            }
+            else
+            {
+                Console.WriteLine("Largest: " + c);
+            }
+        }
+        else
+        {
+            if (b > c)
+            {
+                Console.WriteLine("Largest: " + b);
+            }
+            else
+            {
+                Console.WriteLine("Largest: " + c);
+            }
+        }
+    }
+}
+                  
 
