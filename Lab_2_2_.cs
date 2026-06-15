f5687a414bd616209cfb1c276fadc3e65798d0e9
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-06-15 06:12:30 +0545

    Add number classification in Lab_2_2_.cs
    test whether given number is positive or negative or Zero.

--- Lab_2_2_.cs
+++ Lab_2_2_.cs
@@ -1,1 +1,25 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        int num;
+
+        Console.Write("Enter number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num > 0)
+        {
+            Console.WriteLine("Positive");
+        }
+        else if (num < 0)
+        {
+            Console.WriteLine("Negative");
+        }
+        else
+        {
+            Console.WriteLine("Zero");
+        }
+    }
+}
