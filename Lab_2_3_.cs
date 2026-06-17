ef20f0f88353727973bfbe2046303da8588b09c3
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-06-17 06:40:24 +0545

    Update Lab_2_3_.cs

--- Lab_2_3_.cs
+++ Lab_2_3_.cs
@@ -1,1 +1,46 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        int a, b, c;
+
+        Console.Write("Enter the first number: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the third number: ");
+        c = Convert.ToInt32(Console.ReadLine());
+
+        // Nested If-Else
+        if (a > b)
+        {
+            if (a > c)
+            {
+                Console.WriteLine("Largest: " + a);
+            }
+            else
+            {
+                Console.WriteLine("Largest: " + c);
+            }
+        }
+        else
+        {
+            if (b > c)
+            {
+                Console.WriteLine("Largest: " + b);
+            }
+            else
+            {
+                Console.WriteLine("Largest: " + c);
+            }
+        }
+    }
+}
+                  
+                    
+                    
+                  
