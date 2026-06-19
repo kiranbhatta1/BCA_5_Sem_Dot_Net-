94ccd8b3e287375b59a8b238aeac09d43e4ff3f5
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-06-19 07:56:39 +0545

    Implement prime number checking functionality
    Added a program to check if a number is prime.

--- Lab_2_5_.cs
+++ Lab_2_5_.cs
@@ -1,1 +1,39 @@
+using System;
 
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter a Number:");
+        int n = int.Parse(Console.ReadLine());
+
+        bool isPrime = true;
+
+        if (n <= 1)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+        {
+            Console.WriteLine("The Number is Prime.");
+        }
+        else
+        {
+            Console.WriteLine("The Number is Not Prime.");
+        }
+    }
+}
+    
+        
