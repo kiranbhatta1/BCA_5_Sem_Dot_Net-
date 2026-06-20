8a66d4d1ccb182b8d47a205613445763f55ea918
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-06-20 22:18:23 +0545

    Implement Armstrong number check in Lab_2_6_.cs

--- Lab_2_6_.cs
+++ Lab_2_6_.cs
@@ -1,1 +1,30 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        int num, sum = 0, temp, rem;
+
+        Console.Write("Enter a number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        temp = num;
+
+        while (temp != 0)
+        {
+            rem = temp % 10;
+            sum += rem * rem * rem;
+            temp /= 10;
+        }
+
+        if (sum == num)
+        {
+            Console.WriteLine("Armstrong Number");
+        }
+        else
+        {
+            Console.WriteLine("Not Armstrong Number");
+        }
+    }
+}
