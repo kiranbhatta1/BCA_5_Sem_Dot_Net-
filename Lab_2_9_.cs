196cc338a4ac7742879d263af74091007fd7e322
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-06-28 08:50:01 +0545

    Implement sum calculation for non-negative numbers

--- Lab_2_9_.cs
+++ Lab_2_9_.cs
@@ -1,1 +1,21 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        int num, sum = 0;
+
+        do
+        {
+            Console.Write("Enter Number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num >= 0)
+                sum += num;
+
+        } while (num >= 0);
+
+        Console.WriteLine("Sum = " + sum);
+    }
+}
