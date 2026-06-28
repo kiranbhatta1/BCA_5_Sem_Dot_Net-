32c04330d85e198794ecdd6b3af694429882158d
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-06-28 11:04:55 +0545

    Add program to calculate cubes of numbers

--- Lab_2_10_.cs
+++ Lab_2_10_.cs
@@ -1,1 +1,20 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        int n, i = 1;
+
+        Console.Write("Enter Number: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+    Start:
+        if (i <= n)
+        {
+            Console.WriteLine(i + "^3 = " + (i * i * i));
+            i++;
+            goto Start;
+        }
+    }
+}
