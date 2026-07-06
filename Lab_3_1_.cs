fc8b468c507db7633d8101ac4b10cdc93402240e
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-07-06 08:08:26 +0545

    Add program to calculate sum of odd elements

--- Lab_3_1_.cs
+++ Lab_3_1_.cs
@@ -1,1 +1,27 @@
+using System;
 
+class Program1
+{
+    static void Main()
+    {
+        int n, sum = 0;
+
+        Console.Write("Enter Size: ");
+        n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Enter Element: ");
+            arr[i] = int.Parse(Console.ReadLine());
+
+            if (arr[i] % 2 != 0)
+            {
+                sum += arr[i];
+            }
+        }
+
+        Console.WriteLine("Sum of Odd Elements = " + sum);
+    }
+}
