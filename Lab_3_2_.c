bfde617dc12530ae915dc507e6b84a201bbf6b65
Kiran Bhatta <bhattakiran1111@gmail.com>
2026-07-09 13:58:46 +0545

    Implement min and max calculation for user input array
    The program prompts the user to enter the size of an array and its elements, then calculates and displays the minimum and maximum values from the input.

--- Lab_3_2_.c
+++ Lab_3_2_.c
@@ -1,1 +1,230 @@
+using System;
+
+class Program2
+{
+    static void Main()
+    {
+        int n;
+
+        Console.Write("Enter Size: ");
+        n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[5];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Enter Element: ");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        int min = arr[0], max = arr[0];
+
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] < min)
+                min = arr[i];
+
+            if (arr[i] > max)
+                max = arr[i];
+        }
+
+        Console.WriteLine("Minimum = " + min);
+        Console.WriteLine("Maximum = " + max);
+    }
+}
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
